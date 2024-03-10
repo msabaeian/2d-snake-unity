4a8c6a26b7ccf1136a6c3cc8d2606d61b905bc15
Mohammad Sabaeian <msabaeian@gmail.com>
2024-03-10 23:43:34 +0100

    add inputs and player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _direction;
+    private float speed = 5f;
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void OnMovement(InputValue value)
+    {
+        Vector2 newValue = value.Get<Vector2>();
+        if (newValue is { x: 0, y: 0 } || (newValue.x != 0 && newValue.y != 0))
+        {
+            return;
+        }
+        _direction = newValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = _direction * speed;
     }
 }
